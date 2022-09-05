0ef248b29ac860990c7f3bc3ff574fb315596198
zYoru <xininfected@gmail.com>
2022-09-05 22:29:15 +0200

    todo reaction fix

--- src/main/java/de/lecuutex/discordbot/listener/GuildMessageReactionAddListener.java
+++ src/main/java/de/lecuutex/discordbot/listener/GuildMessageReactionAddListener.java
@@ -6,6 +6,7 @@ import jdk.jshell.execution.Util;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -23,9 +24,11 @@ public class GuildMessageReactionAddListener extends ListenerAdapter {
     @Override
     public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
         if (event.getChannel().getId().equals("940634940591202314")) {
-            if (!event.getUser().getId().equals(Utils.DANIEL_ID) && !event.getUser().isBot()) {
+            User user = event.getUser();
+
+            if (!user.getId().equals(Utils.DANIEL_ID) && !user.isBot()) {
                 event.getReaction().removeReaction().queue();
-            } else if (event.getReactionEmote().getName().equals("✅") && event.getUser().getId().equals(Utils.DANIEL_ID)) {
+            } else if (event.getReactionEmote().getName().equals("✅") && user.getId().equals(Utils.DANIEL_ID)) {
                 Message botMessage = event.getReaction().getTextChannel().retrieveMessageById(event.getMessageId()).complete();
                 String content = "";
 
@@ -34,8 +37,8 @@ public class GuildMessageReactionAddListener extends ListenerAdapter {
                 }
 
                 new Embed("⛏ **|** History", Utils.PREFIX + "Datum: "
-                                + new SimpleDateFormat().format(new Date(botMessage.getTimeCreated().toInstant().getEpochSecond() * 1000))
-                                + "\n\n" + content, Color.decode("#6c63cd"))
+                        + new SimpleDateFormat().format(new Date(botMessage.getTimeCreated().toInstant().getEpochSecond() * 1000))
+                        + "\n\n" + content, Color.decode("#6c63cd"))
                         .send(event.getGuild().getTextChannelById("947588938284027914"));
 
                 botMessage.delete().queue();
